f15ba6ff760a806efb6ed734467769eec3fa7530
ajaz-ayaz <156538611+ajaz-ayaz@users.noreply.github.com>
2024-01-20 22:45:12 +0500

    Add files via upload

---
+++ Muhammad_Ajaz_Midterm.py
@@ -0,0 +1,62 @@
+
+# MUHAMMAD AJAZ AYAZ
+# ID NO # 63410
+
+
+# Question Number 1: Write a Python program to do arithmetical operations addition and division.
+
+num1 = int(input("Enter the first number for addition:"))
+num2 = int(input("Enter the second number for addition:"))
+answer = num1 + num2
+print( " additon: " , (num1) , " + " , (num2) , " = " , answer)
+
+num1 = int(input("Enter the dividend for division:"))
+num2 = int(input("Enter the divisor for division:"))
+answer = num1 / num2
+print( " Division " , (num1) , " / " , (num2) , " = " , answer)
+
+
+num1 = int(input("Enter number:"))
+num2 = int(input("Enter power:"))
+answer = num1 ** num2
+print( " Result : " , answer)
+
+
+num1 = int(input("Enter Divident:"))
+num2 = int(input("Enter Divisor:"))
+answer = num1 /num2
+print( (num2) , " // " , (answer))
+print( " Result " , answer)
+
+
+# Question Number 2 : Write a Python program to find the area of a triangle.
+
+num1 = int(input("Enter the lenght of the base of the triangle:"))
+num2 = int(input("Enter the hight of the triangle:"))
+answer = num1 * num2 / 2
+print( "The area of triangle is: " , answer)
+
+
+# Question Number 3: Write a Python program to convert Celsius to Fahrenheit.
+
+num1 = int(input("Enter temperature in celsius:"))
+answer = 9 / 5 * num1 + 32
+print("degree celsius is equal to: " , answer , "degrees Fahrenheit")
+
+
+# Question Number 4 : Write a Python program that return all datatypes that we discussed in the class.
+
+a = 19
+print( "the data type of " , (a) , " is " , type(a))
+b = 16.5
+print( "the data type of  " , (b) , " is " , type(b))
+c = ("AJAZ")
+print( "the data type of " , (c) , "is" , type(c))
+d = True
+print( "the data type of  " , (d) , "is" , type(d))
+e = [1,2,3]
+print( "the data type of  " , (e) , "is" , type(e))
+f = (1,2,3)
+print( "the data type of  " , (f) , "is" , type(f))
+g = {1,2,3}
+print( "the data type of  " , (g) , "is" , type(g))
